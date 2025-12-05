fb3836e5e40c2ddc3ea797555c04a4291e1fa125
SinoheGalvan <jose_3141240116@utd.edu.mx>
2025-12-04 22:29:10 -0600

    commit_04_12_25

--- P3/PROYECTOS_TKINTER/coches_system/controller/controlador1.py
+++ P3/PROYECTOS_TKINTER/coches_system/controller/controlador1.py
@@ -13,11 +13,19 @@ class Controller:
 
     @staticmethod
     def consultar_id_auto(id):
-        try:
-            cursor.execute("select * from autos where id_coche=%s",(id,))
-            return cursor.fetchone()
-        except:
-            return []
+            try:
+                cursor.execute("select * from autos where id_coche=%s",(id,))
+                return cursor.fetchone()
+            except:
+                return []
+
+    @staticmethod
+    def consultar_id_camioneta(id):
+            try:
+                cursor.execute("select * from camionetas where id_camioneta=%s",(id,))
+                return cursor.fetchone()
+            except:
+                return []
 
     @staticmethod
     def insertar_auto(marca,color,modelo,velocidad,caballaje,num_plazas):
@@ -25,7 +33,7 @@ class Controller:
         Controller.respuesta_sql(respuesta)
 
     @staticmethod
-    def consultar_auto(ventana):
+    def consultar_autos(ventana):
         registros = cochesBD.Autos.consultar()
 
         columnas = ["ID","Marca","Color","Modelo","velocidad","Caballaje","Plazas"]
@@ -51,4 +59,38 @@ class Controller:
     @staticmethod
     def eliminar_auto(id):
         respuesta = cochesBD.Autos.eliminar(id)
+        Controller.respuesta_sql(respuesta)
+
+    @staticmethod
+    def insertar_camioneta(marca,color,modelo,velocidad,caballaje,num_plazas,traccion,cerrada):
+        respuesta = cochesBD.Camionetas.insertar(marca,color,modelo,velocidad,caballaje,num_plazas,traccion,cerrada)
+        Controller.respuesta_sql(respuesta)
+
+    @staticmethod
+    def consultar_camionetas(ventana):
+        registros = cochesBD.Camionetas.consultar()
+
+        columnas = ["ID","Marca","Color","Modelo","velocidad","Caballaje","Plazas","Traccion","¿Cerrada?"]
+        tabla = ttk.Treeview(ventana, columns=columnas, show="headings")
+
+        for col in columnas:
+            tabla.heading(col, text=col, anchor=W)
+            if col == "ID":
+                tabla.column(col, anchor=CENTER, width=50)
+            else:
+                tabla.column(col, anchor=W, width=120)
+
+        for i, fila in enumerate(registros):
+            tabla.insert(parent='', index=END, iid=fila[0], values=fila)
+
+        tabla.pack(pady=20, padx=20,fill="both")
+
+    @staticmethod
+    def actualizar_camioneta(marca,color,modelo,velocidad,caballaje,num_plazas,traccion,cerrada,id):
+        respuesta = cochesBD.Camionetas.actualizar(marca,color,modelo,velocidad,caballaje,num_plazas,traccion,cerrada,id)
+        Controller.respuesta_sql(respuesta)
+
+    @staticmethod
+    def eliminar_camioneta(id):
+        respuesta = cochesBD.Camionetas.eliminar(id)
         Controller.respuesta_sql(respuesta)
